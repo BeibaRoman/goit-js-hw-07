f051dcc84b29284e4ecff02846b761c1f2e9af69
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2023-04-25 13:48:11 +0300

    add second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = makeGalleryImgMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function makeGalleryImgMarkup(el) {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
+    })
+    .join("");
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
